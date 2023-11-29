af3c33747d26dcf501577d8a73128a538fdb068c
Johnny18823 <jw3621@ic.ac.uk>
2023-11-29 13:50:44 +0000

    fix some bug

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ChatterBotCorpusTrainer
 import openai
 import streamlit as st
 
